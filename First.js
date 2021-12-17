8e986f6922a6afd2ffa33884217c67a5521ca3ca
Abdul Mujeeb <shaikmujeeb273gmail.com@Shaiks-MacBook-Air.local>
2021-12-17 23:46:52 +0530

    TypeScript Project

---
+++ First.js
@@ -0,0 +1,8 @@
+var mujeeb = "minerva";
+console.log(mujeeb);
+var some = document.querySelectorAll("input");
+console.log(some);
+some.forEach(function (input) {
+    console.log(input);
+});
+console.log(mujeeb);
---
+++ First.ts
@@ -0,0 +1,10 @@
+let mujeeb = "minerva";
+console.log(mujeeb);
+let some = document.querySelectorAll("input");
+console.log(some);
+
+some.forEach((input) => {
+  console.log(input);
+});
+
+console.log(mujeeb);
